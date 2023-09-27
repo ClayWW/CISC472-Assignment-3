456ce4041a2d82a1a9ed5a6dc804453c4038c9a3
Clay <clayw@udel.edu>
2023-09-26 21:57:49 -0400

    Comments on 3

--- question2.py
+++ question2.py
@@ -1,36 +1,40 @@
 from Crypto.Cipher import AES
 
+#our two initial ciphertexts that were intercepted
 ctxt1_hex = "3012e00127813d5a563f775e0906d081"
 ctxt2_hex = "79789673ec9cf766126751c0d7486604"
 
+#ciphertexts in bytes
 ctxt1_bytes = bytes.fromhex(ctxt1_hex)
 ctxt2_bytes = bytes.fromhex(ctxt2_hex)
 
+#double for loop that each go from 0-256 so that we cover all 2^16 possibilites for the two bytes that are repeated in the key
 for byte1_val in range(256):
     for byte2_val in range(256):
-        byte1_bytes = bytes([byte1_val])
+        byte1_bytes = bytes([byte1_val]) #turns the values into bytes
         byte2_bytes = bytes([byte2_val])
 
-        key_bytes = (byte1_bytes + byte2_bytes)*8
+        key_bytes = (byte1_bytes + byte2_bytes)*8 #creates the key based on the bytes, just two bytes repeated 8 times
 
-        cipher = AES.new(key_bytes, AES.MODE_ECB)
+        cipher = AES.new(key_bytes, AES.MODE_ECB) #create the AES cipher utilized in encryption and pass in a key 
 
         try:
-            plaintext1_bytes = cipher.decrypt(ctxt1_bytes)
+            plaintext1_bytes = cipher.decrypt(ctxt1_bytes) #decrypt both ciphertexts 
             plaintext2_bytes = cipher.decrypt(ctxt2_bytes)
 
-            plaintext1 = plaintext1_bytes.decode('iso-8859-1')
+            plaintext1 = plaintext1_bytes.decode('iso-8859-1') #turn the bytes back into plaintext
             plaintext2 = plaintext2_bytes.decode('iso-8859-1')
 
-            if(plaintext1.isprintable() and plaintext2.isprintable()):
-                print("Potential Key Found: ", key_bytes.hex())
+            if(plaintext1.isprintable() and plaintext2.isprintable()): #if the bytes are printable (not gibberish), print them out and see if they make sense
+                print("Potential Key Found: ", key_bytes.hex()) #fully print out everything, both plaintexts need to make sense in order for the key to possibly be correct
                 print("Decrypted Plainext 1: ",plaintext1)
                 print("Decrypted Plaintext 2: ", plaintext2)
                 print("\n")
                 break
-        except Exception as e:
+        except Exception as e: #in case of any errors involving the cipher or decryption
             pass
 
+#RESULTS
 #Key: 1c4d1c4d1c4d1c4d1c4d1c4d1c4d1c4d
 #Plaintext 1: my key is weak!!
 #Plaintext 2: Eve cant read it
--- question3.py
+++ question3.py
@@ -1,6 +1,7 @@
 from main import PRF
 from drbg import *
 
+#our xor function but this time for bits instead of ascii characters
 def xor(X, Y):
     try:
         return bytes(a ^ b for a,b in zip(X,Y))
@@ -8,29 +9,32 @@ def xor(X, Y):
         print(f"Error XOR-ing: {X=} {Y=}")
         raise
 
+#takes in a key and uses it to generate a specified amount of round keys
+#we know that we are passing 8 bytes into the PRF so we make our subkeys 8 bytes long
 def generate_round_keys(key, rounds): #works
     subkeys = []
     drbg = DRBG(key)
-    prb = drbg.generate(rounds*8)
-    for round_num in range(rounds):
+    prb = drbg.generate(rounds*8) #generates an exact amount of psuedorandom bytes
+    for round_num in range(rounds): #divides up the keys
         start = round_num*8
         end = start+8
         subkey = prb[start:end]
         subkeys.append(subkey)
-    return subkeys
+    return subkeys #return the array of subkeys
 
+#divides the blocks based on a specified size and adds padding if necessary
 def divide_blocks(plaintext, block_size_bits: int):
-    block_size_bytes = block_size_bits // 8
-    if isinstance(plaintext, str):
+    block_size_bytes = block_size_bits // 8 #bits to bytes
+    if isinstance(plaintext, str): #ensure that plaintext is in bytes
         plaintext_bytes = plaintext.encode('iso-8859-1')
     elif isinstance(plaintext, bytes):
         plaintext_bytes = plaintext
     else:
-        raise ValueError("input needs to be a string or bytes")
+        raise ValueError("input needs to be a string or bytes") #in case incorrect input
     
-    blocks = [plaintext_bytes[i:i+block_size_bytes] for i in range(0, len(plaintext_bytes), block_size_bytes)]
-    last_block_len = len(blocks[-1])
-    if(last_block_len < block_size_bytes):
+    blocks = [plaintext_bytes[i:i+block_size_bytes] for i in range(0, len(plaintext_bytes), block_size_bytes)] #divide the blocks and store in array
+    last_block_len = len(blocks[-1]) 
+    if(last_block_len < block_size_bytes): #pads the final block if necessary, adds more than necessary padding in some cases
         padding_length = block_size_bytes - last_block_len
         padding = bytes([padding_length]) * padding_length
         blocks[-1] += padding
@@ -40,87 +44,94 @@ def divide_blocks(plaintext, block_size_bits: int):
         padding_length = block_size_bytes
     else:
         padding_length = 0
-    return blocks, padding_length
+    return blocks, padding_length #returns the array of blocks and the amount of padding on the final block
 
+#takes in a block and removes additional padding
 def remove_padding(block):
-    padding_length = block[-1]
-    if padding_length == len(block):
+    padding_length = block[-1] #specify how many bytes to delete
+    if padding_length == len(block): # if final block is full padding just delete the block
         return b""
-    return block[:-padding_length]
+    return block[:-padding_length] #return the unpadded block
 
-def bh_encrypt(plaintext: str, key: bytes, rounds: int): #will finish with all blocks being full
+#encrypts a plaintext through a feistel network using an inputted key and inputted amount of rounds
+def bh_encrypt(plaintext: str, key: bytes, rounds: int): 
     encrypted_blocks = []
     subkeys = []
-    blocks, padding_length = divide_blocks(plaintext, 128)
-    block: bytes
-    for block in blocks:
-        left_bytes,right_bytes = block[:8],block[8:]  #correctly splits
-        if not isinstance(right_bytes, bytes):
+    blocks, padding_length = divide_blocks(plaintext, 128) #divide plaintext into blocks
+    block: bytes #suggest block as type bytes
+    for block in blocks: #for each block
+        left_bytes,right_bytes = block[:8],block[8:]  #divide block in half
+        if not isinstance(right_bytes, bytes): #ensure left half is in bytes
             right_bytes = right_bytes.encode('iso-8859-1')
-        if not isinstance(left_bytes, bytes):
+        if not isinstance(left_bytes, bytes): #ensure right half is in bytes
             left_bytes = left_bytes.encode('iso-8859-1')
-        subkeys = generate_round_keys(key, rounds) #correctly generates keys, wrong size maybe?
+        subkeys = generate_round_keys(key, rounds) #generate subkey for each round
 
-        for round in range(rounds):
-            prf_r = PRF(subkeys[round],right_bytes) #feed the right side in bytes through the PRF
-            xored_left = xor(left_bytes, prf_r) #pass the bytes in as strings of english for xor
-            left_bytes = right_bytes #does not change
-            right_bytes = xored_left
+        for round in range(rounds): #perform feistel scheme for amount of rounds specifed (0,1,2,3,4)
+            prf_r = PRF(subkeys[round],right_bytes) #feed the right side in bytes through the PRF with unique subkey
+            xored_left = xor(left_bytes, prf_r) #xor the left bytes with the results of the PRF
+            left_bytes = right_bytes #the bytes that were once the left half become the right
+            right_bytes = xored_left #the bytes that were once the right half become the result of XOR operation between left bytes and PRF output
 
-        ciphertext = left_bytes+right_bytes
-        encrypted_blocks.append(ciphertext)
+        ciphertext = left_bytes+right_bytes #combine left and right into the final block
+        encrypted_blocks.append(ciphertext) #add the encrypted block into the encrypted blocks array
 
-    concat_blocks = b"".join(encrypted_blocks)
-    return concat_blocks, subkeys, padding_length
+    concat_blocks = b"".join(encrypted_blocks) #combine all of the encrypted blocks and return
+    return concat_blocks, subkeys, padding_length #return ciphertext, the subkeys used in all the rounds, and any padding the final block of ciphertext may have
 
-
+#decrypts ciphertext through a feistel network by passing the ciphertext through for the same amount of rounds as encryption but subkeys are applied in reverse order
 def bh_decrypt(ciphertext, subkeys, rounds, padding_length):
     decrypted_blocks = []
-    blocks, padding_length = divide_blocks(ciphertext, 128)
-    for block in blocks:
-        left,right = block[:8],block[8:]
-        for round in range(rounds-1, -1, -1): #working
-            prf_r = PRF(subkeys[round],right)
-            xored_left = xor(left, prf_r)
-            left = right
-            right = xored_left
-        plaintext = left+right
-        if block == len(blocks)-1:
+    blocks, padding_length = divide_blocks(ciphertext, 128) #divide ciphertext into blocks
+    for block in blocks: #for every block
+        left,right = block[:8],block[8:] #divide the block into left and right
+        for round in range(rounds-1, -1, -1): #loop through in reverse order (4,3,2,1,0)
+            prf_r = PRF(subkeys[round],right) #apply inverse subkey for the PRF
+            xored_left = xor(left, prf_r) #perform the same xor operation between left bytes and the output of the prf
+            left = right #left bytes are now the right bytes
+            right = xored_left #right bytes are now the ouput of the xor operation
+        plaintext = left+right #recombine block
+        if block == len(blocks)-1: #check for padding and remove
             plaintext = plaintext[:-padding_length]
-        decrypted_blocks.append(plaintext)
-    concat_blocks = b"".join(decrypted_blocks)
+        decrypted_blocks.append(plaintext) #add back decrypted block to array
+    concat_blocks = b"".join(decrypted_blocks) #combine decrypted blocks and return decrypted ciphertext
     return concat_blocks
 
+#performs ctr encryption using the bh_encryption scheme above.
+#Takes in a message in bytes format, a key, the nonce used, and the amount of rounds executed in the fesitel network
 def bh_ctr_encryption(message_bytes, key, nonce, rounds):
     encrypted_blocks = []
-    blocks, _ = divide_blocks(message_bytes, 128)
-    for i,block in enumerate(blocks):
-        if(i > 256):
+    blocks, _ = divide_blocks(message_bytes, 128) #divide message into blocks
+    for i,block in enumerate(blocks): #i acts as the counter, increments with every block
+        if(i > 256): #if counter is maxed out indicate the need for a new nonce
             print("Out of counter, need a new nonce!")
             return
-        input = nonce+i.to_bytes(8,'big')
-        encrypted,_,_ = bh_encrypt(input, key, rounds)
-        ciphertext = xor(block, encrypted)
-        encrypted_blocks.append(ciphertext)
-    concat_ciphertext = b"".join(encrypted_blocks)
+        input = nonce+i.to_bytes(8,'big') #concatenate the nonce and counter
+        encrypted,_,_ = bh_encrypt(input, key, rounds) #encrypt the nonce+counter using the bh_encrypt function
+        ciphertext = xor(block, encrypted) #xor the encrypted nonce+counter with the block of the message
+        encrypted_blocks.append(ciphertext) #add the encrypted block to the array
+    concat_ciphertext = b"".join(encrypted_blocks) #combine all encrypted blocks and return the ciphertext
     return concat_ciphertext
 
-
+#I also implemented ctr decryption so i could make sure the ctr encryption worked properly (FEEL FREE TO IGNORE)
+#Takes in the ciphertext from the encryption, the key used, the nonce used, the amount fo rounds in bh_encrypt, and the potential padding on the last block
+#Pretty much the same as encryption with a couple changes
 def bh_ctr_decryption(ciphertext, key, nonce, rounds, padding_length): 
     decrypted_blocks = []
-    blocks, _ = divide_blocks(ciphertext, 128) #correctly divides
+    blocks, _ = divide_blocks(ciphertext, 128) #correctly divides ciphertext
     for i,block in enumerate(blocks): 
         if(i > 256):
             print("Out of counter, Need a new nonce!")
             return
         input = nonce+i.to_bytes(8,'big')
         encrypted, _, _ = bh_encrypt(input, key, rounds)
-        message = xor(block, encrypted)
-        if(i == len(blocks)-1):
+        message = xor(block, encrypted) #xor the ciphertext and the encrypted nonce+counter
+        if(i == len(blocks)-1): #check for padding on the final block and remove
             message = message[:-padding_length]
         decrypted_blocks.append(message)
-    concat_plain = b"".join(decrypted_blocks) #empty when it shouldn't be
+    concat_plain = b"".join(decrypted_blocks)
     return concat_plain.decode('iso-8859-1')
+
 
 def bh_cbc_encryption(plaintext, key, IV, rounds):
     encrypted_blocks = []
@@ -128,65 +139,61 @@ def bh_cbc_encryption(plaintext, key, IV, rounds):
     prev_cipher_block = IV
     for block in blocks:
         xor_block = xor(block, prev_cipher_block)
-        print("XORed Block (Encryption):", xor_block.hex())
         encrypted_block,_,_ = bh_encrypt(xor_block, key, rounds)
         encrypted_blocks.append(encrypted_block)
         prev_cipher_block = encrypted_block
-        print("Encrypted Block:", encrypted_block.hex()) 
     concat_ciphertext = b"".join(encrypted_blocks)
     return concat_ciphertext
 
-'''
-def bh_cbc_decryption(ciphertext, key, IV, rounds):
-    decrypted_blocks = []
-    blocks, padding_length = divide_blocks(ciphertext, 128)
-    subkeys = generate_round_keys(key, rounds)
-    prev_cipher_block = IV
-    for block in blocks:
-        decrypted_block = bh_decrypt(block, subkeys, rounds, padding_length)
-        print("Decrypted Block (Before XOR):", decrypted_block.hex())
-        xor_block = xor(decrypted_block, prev_cipher_block)
-        decrypted_blocks.append(xor_block)
-        prev_cipher_block = block
-        print("XORed Block (Decryption):", xor_block.hex())
-    decrypted_blocks[-1] = remove_padding(decrypted_blocks[-1])
-    concat_plaintext = b"".join(decrypted_blocks)
-    return remove_padding(concat_plaintext)
-'''
     
 #testing feistel
+#3a Questions
+#Each subkey should be 8 bytes, or half the length of the block
+#You need a different subkey for each round, so we will generate 5 subkeys
+
 plaintext = "Hello, World CWW"
+plaintext2 = "This is an attempt to see if a longer plaintext yields better results"
+
 key = bytes.fromhex("7bc6ac0dbe97d6e41cb440abd82b8dcf")
 rounds = 5
+
+print("Plaintext 1: ", plaintext)
 ciphertext, subkeys, padding_length = bh_encrypt(plaintext, key, rounds)
-print(ciphertext.hex())
+print("Ciphertext 1: ",ciphertext.hex())
 decrypted_plaintext = bh_decrypt(ciphertext, subkeys, rounds, padding_length)
-print(decrypted_plaintext.decode('iso-8859-1'))
+print("Decrypted Ciphertext 1: ",decrypted_plaintext.decode('iso-8859-1'))
+
+print("Plaintext 2: ", plaintext2)
+ciphertext2, subkeys2, padding_length2 = bh_encrypt(plaintext2, key, rounds)
+print("Ciphertext 2: ",ciphertext2.hex())
+decrypted_plaintext2 = bh_decrypt(ciphertext2, subkeys2, rounds, padding_length2)
+print("Decrypted Ciphertext 2: ", decrypted_plaintext2.decode('iso-8859-1'))
+print("\n")
 
 #testing bh_ctr_decryption
-message = "0123456789ABCDEF0123456789ABCDEF"
-message2 = "Clay got this"
+#Questions for 3b
+#When you are using the same key, you need to change the nonce for every message
+#The decryption algorithm needs the ciphertext, the key, the IV, and in our case the amount of rounds used in the feistel scheme
+#You do not need to add padding for this mode of operation
+message = "This is my attempt to encrypt a string that is four blocks long!"
 message_bytes = message.encode('iso-8859-1')
-message2_bytes = message2.encode('iso-8859-1')
 nonce = bytes.fromhex("59733285e8d82615")
+print("Plaintext: ",message)
 ciphertext_ctr = bh_ctr_encryption(message_bytes, key, nonce, rounds)
-ciphertext_ctr_2 = bh_ctr_encryption(message2_bytes, key, nonce, rounds)
-
+print("Ciphertext: ",ciphertext_ctr.hex())
 concat_plain = bh_ctr_decryption(ciphertext_ctr, key, nonce, rounds, padding_length)
-concat_plain2 = bh_ctr_decryption(ciphertext_ctr_2, key, nonce, rounds, padding_length)
-print(message)
-print(concat_plain)
-print(message2)
-print(concat_plain2)
+print("Should match with plaintext: ",concat_plain)
+print("\n")
 
 #testing cbc encryption
-plaintext = "Time to test CBC encryption"
+#Questions for 3c
+#For CBC decryption, you need the ciphertext, the key, the IV, and because we are using a feistel scheme for encryption we need to also provide the amount of rounds used
+plaintext = "Attempting to encrypt three blocks of data now!!"
 IV = bytes.fromhex("1218a9f78bf8bedff5013407dc712544")
 cipher = bh_cbc_encryption(plaintext, key, IV, rounds)
-print("Encrypted: ", cipher.hex())
+print("Plaintext: ",plaintext)
+print("Ciphertext: ", cipher.hex())
+print("\n")
 
-#testing cbc decryption
-#decrypted = bh_cbc_decryption(cipher, key, IV, rounds)
-#print("Decrypted: ", decrypted.decode('iso-8859-1'))
 
 
